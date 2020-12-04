3bc9ca9e65e2b7c82d6aacb209de1a63d57aa873
homeless-coder <luisferpo13@gmail.com>
2020-12-04 15:35:36 -0600

    I have implemented Input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,36 @@
+
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { TextField, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  input: {
+    backgroundColor: "#f1f2f2",
+    width: "35%",
+    borderRadius: 6,
+  },
+  label: {
+    color: "#B8B8B8",
+    fontSize: "1.2em",
+  },
+}));
+
+const Input = ({ value, onChange, label, id }) => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <TextField
+        error={false}
+        value={value}
+        id={id}
+        className={classes.input}
+        variant="outlined"
+        onChange={(e) => onChange(e)}
+      />
+      <Typography className={classes.label}>{label}</Typography>
+    </>
+  );
+};
+
+export default Input;
