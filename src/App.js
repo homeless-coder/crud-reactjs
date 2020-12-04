f40202d06bb4b151e30a1f3843e0ca54ab015654
homeless-coder <luisferpo13@gmail.com>
2020-12-04 15:26:28 -0600

    I have implemented the Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar } from "./components";
+import { Navbar, Footer } from "./components";
 import "./App.css";
 
 function App() {
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar title="CRUD" />
+      <Footer />
     </div>
   );
 }
